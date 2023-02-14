dd60177cc4f1a434a034d0bd454057dd45e59d8d
Cemal Yiğit <Yigit.celi@outlook.com>
2023-02-14 19:24:54 +0300

    Pages Setup

---
+++ src/Pages/Login.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SgLogo from '../assets/OpenAI-Single.logo.png';
+
+function Login() {
+  return (
+    <div className='w-full h-full flex items-center justify-center bg-[#343541]'><img src={SgLogo}/></div>
+  )
+}
+
+export default Login
---
+++ src/Pages/index.ts
@@ -0,0 +1,8 @@
+import Login from "./Login";
+import Homepage from './Homepage';
+
+
+export {
+    Login,
+    Homepage
+}
